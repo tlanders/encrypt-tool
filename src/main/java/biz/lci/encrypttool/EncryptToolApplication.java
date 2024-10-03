86438f4dff27096d03ef75f22cc0183adcff77b2
Troy Landers <tlanders@landersconsulting.biz>
2024-10-03 09:44:00 -0500

    initial commit

---
+++ src/main/java/biz/lci/encrypttool/EncryptToolApplication.java
@@ -0,0 +1,13 @@
+package biz.lci.encrypttool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncryptToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncryptToolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/biz/lci/encrypttool/EncryptToolApplicationTests.java
@@ -0,0 +1,13 @@
+package biz.lci.encrypttool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncryptToolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
